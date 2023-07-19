cc94062f69579bb8e119e6139003d08c6c72d0f8
Nishant Parashar <nishantparashar@Nishants-MacBook-Air.local>
2023-07-19 12:05:22 +0530

    added stream

---
+++ README.md
@@ -0,0 +1,9 @@
+# Agora Web UIKit RTMP
+
+Stream Agora Live stream to a CDN using RTMP streaming. This project is built with React and the [Agora Web UIKit](https://github.com/AgoraIO-Community/Web-React-UIKit/).
+
+## How To Use
+Add your Agora App ID to `./src/App.tsx` and your RTMP URL to `./src/RTMP.tsx`.  
+You can start the project by executing `npm i && npm start`.  
+
+This is a good example to understand how to recompose the Web UIKit components to add a custom feature to your website built using the UIKit.
---
+++ src/App.tsx
@@ -0,0 +1,81 @@
+import React, { useContext, useState } from 'react'
+import { PropsContext, GridVideo, layout, LocalControls, PinnedVideo, RtcConfigure, TracksConfigure } from 'agora-react-uikit'
+import 'agora-react-uikit/dist/index.css'
+import RTMP from './RTMP'
+
+const App: React.FunctionComponent = () => {
+  const [videocall, setVideocall] = useState(true)
+  const [isHost, setHost] = useState(true)
+  const [isPinned, setPinned] = useState(false)
+
+  return (
+    <div style={styles.container}>
+      <div style={styles.videoContainer}>
+        <h1 style={styles.heading}>Agora React Web UI Kit</h1>
+        {videocall ? (<>
+          <div style={styles.nav}>
+            <p style={{ fontSize: 20, width: 200 }}>You're {isHost ? 'a host' : 'an audience'}</p>
+            <p style={styles.btn} onClick={() => setHost(!isHost)}>Change Role</p>
+            <p style={styles.btn} onClick={() => setPinned(!isPinned)}>Change Layout</p>
+          </div>
+          <PropsContext.Provider value={{
+            rtcProps: {
+              appId: process.env.REACT_APP_APPID || '526b5d98970e4093b953bd95e54a737a',
+              channel: process.env.REACT_APP_CHANNEL || 'nis',
+              token: '007eJxTYFgef8v3R4zu8f65YjsiXLc3f/nALRLu5H9mEbMrQ76gKpMCg6mRWZJpiqWFpblBqomBpXGSpalxUoqlaaqpSaK5sXkie/G2lIZARobFpocYGKEQxGdmyMssZmAAAIe7HDg=',
+              role: isHost ? 'host' : 'audience',
+              layout: isPinned ? layout.pin : layout.grid
+            },
+            styleProps: {
+              gridVideoContainer: { 'height': '90%' },
+              pinnedVideoContainer: { 'height': '90%' }
+            },
+            callbacks: {
+              EndCall: () => setVideocall(false),
+            }
+          }} >
+
+            <div style={{ flex: 1 }} >
+              {isHost === false ? (
+                <VideocallUI />
+              ) : (
+                <TracksConfigure>
+                  <VideocallUI />
+                </TracksConfigure>
+              )}
+            </div>
+          </PropsContext.Provider>
+        </>
+        ) : (
+          <div style={styles.nav}>
+            <h3 style={styles.btn} onClick={() => setVideocall(true)}>Start Call</h3>
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}
+
+const VideocallUI = () => {
+  const { rtcProps } = useContext(PropsContext)
+  return (
+    <RtcConfigure callActive={rtcProps.callActive}>
+      <div style={styles.containerInner}>
+        <RTMP />
+      </div>
+      {rtcProps?.layout === layout.grid ? <GridVideo /> : <PinnedVideo />}
+      <LocalControls />
+    </RtcConfigure>
+  )
+}
+
+const styles = {
+  container: { width: '100vw', height: '100vh', display: 'flex', flex: 1, backgroundColor: '#007bff22' },
+  heading: { textAlign: 'center' as const, marginBottom: 0 },
+  videoContainer: { display: 'flex', flexDirection: 'column', flex: 1 } as React.CSSProperties,
+  nav: { display: 'flex', justifyContent: 'space-around' },
+  btn: { backgroundColor: '#007bff', cursor: 'pointer', borderRadius: 5, padding: 5, color: '#ffffff', fontSize: 20 },
+  containerInner: {display: 'flex', flex: 1, alignContent: 'center', alignItems: 'center', marginBottom: 10}
+}
+
+export default App
